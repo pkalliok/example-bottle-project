b354de4d96a2b02d7a0de25ceac009df2c3843d7
Panu Kalliokoski <panu.kalliokoski@csc.fi>
2015-09-08 19:58:59 +0100

    done 0002: deployment rules in makefile

--- makefiles/deploy.mk
+++ makefiles/deploy.mk
@@ -3,12 +3,15 @@ DEPLOY_USER=atehwa
 DEPLOY_SERVER=aulis.sange.fi
 SSH_DEST=$(DEPLOY_USER)@$(DEPLOY_SERVER)
 
-.PHONY: deploy
+.PHONY: deploy undeploy
 
 deploy: source/example-flask.py source/restart-example-flask \
 		stamps/install-depends
 	rsync $^ $(SSH_DEST):example-flask/
-	ssh $(SSH_DEST) example-flask/restart-example-flask
+	ssh $(SSH_DEST) example-flask/restart-example-flask start &
+
+undeploy:
+	ssh $(SSH_DEST) example-flask/restart-example-flask clean
 
 stamps/install-depends:
 	ssh $(SSH_DEST) -t sudo apt-get install python python-flask
