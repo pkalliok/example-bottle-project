136e7acbbb8694eeedb404499234cb7507be3394
Panu Kalliokoski <panu.kalliokoski@csc.fi>
2015-09-09 08:48:25 +0300

    update Debian image

--- makefiles/develop.mk
+++ makefiles/develop.mk
@@ -48,19 +48,19 @@ stamps/docker-image-%: dockerfiles/%.docker stamps/docker-image-minimal
 # Run this on your RHEL6 workstation if you want to setup a development
 # environment on a virtual host (Debian Jessie 8).
 DEVEL_HOST=testdevelhost-1
-stamps/virtualbox-setup: debian-8.1.0-i386-CD-1.iso
-	sudo yum localinstall http://download.virtualbox.org/virtualbox/5.0.2/VirtualBox-5.0-5.0.2_102096_el6-1.x86_64.rpm
+stamps/virtualbox-setup: debian-8.2.0-i386-CD-1.iso
+	#sudo yum localinstall http://download.virtualbox.org/virtualbox/5.0.2/VirtualBox-5.0-5.0.2_102096_el6-1.x86_64.rpm
 	vboxmanage createvm -name $(DEVEL_HOST) -register
 	vboxmanage modifyvm $(DEVEL_HOST) --memory 768 --vram 64 --acpi on --boot1 dvd --nic1 bridged --bridgeadapter1 eth0 --ostype Debian
 	vboxmanage createvdi --filename $(DEVEL_HOST)-disk01.vdi --size 8192 
 	vboxmanage storagectl $(DEVEL_HOST) --name 'IDE Controller' --add ide
 	vboxmanage modifyvm $(DEVEL_HOST) --boot1 dvd --hda $(DEVEL_HOST)-disk01.vdi --sata on
 	vboxmanage storageattach $(DEVEL_HOST) --storagectl 'IDE Controller' --port 0 --device 0 --type hdd --medium $(DEVEL_HOST)-disk01.vdi 
-	vboxmanage storageattach $(DEVEL_HOST) --storagectl 'IDE Controller' --port 1 --device 0 --type dvddrive --medium debian-8.1.0-i386-CD-1.iso 
-	vboxmanage modifyvm $(DEVEL_HOST) --dvd debian-8.1.0-i386-CD-1.iso 
+	vboxmanage storageattach $(DEVEL_HOST) --storagectl 'IDE Controller' --port 1 --device 0 --type dvddrive --medium debian-8.2.0-i386-CD-1.iso
+	vboxmanage modifyvm $(DEVEL_HOST) --dvd debian-8.2.0-i386-CD-1.iso
 	vboxmanage startvm $(DEVEL_HOST)
 	touch $@
 
-debian-8.1.0-i386-CD-1.iso:
-	wget http://ftp.funet.fi/pub/linux/mirrors/debian-cdimage/8.1.0/i386/iso-cd/debian-8.1.0-i386-CD-1.iso
+debian-8.2.0-i386-CD-1.iso:
+	wget http://trumpetti.atm.tut.fi/debian-cd/8.2.0/i386/iso-cd/debian-8.2.0-i386-CD-1.iso
 
